b946a3b9b2e48539caf01d1232cba46e90c5b6f6
erickdominguez <47347282+erickdominguez@users.noreply.github.com>
2020-11-12 10:34:01 -0700

    Actividad 21

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Parrafo from './components/parrafo'
 import Contador from './components/contador'
+import User from './components/User'
 function App() {
   return (
     <div className="App">
       <h1>lorem ipssum</h1>
       <Parrafo></Parrafo>
       <Contador></Contador>
+      <User></User>
     </div>
   );
 }
