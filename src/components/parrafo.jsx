6db9a30f25777d62b039b444a35228c5d4012091
erickdominguez <47347282+erickdominguez@users.noreply.github.com>
2020-11-12 10:20:41 -0700

    aCTIVIDAD 20 CRE0

---
+++ src/components/parrafo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const parrafo = () => {
+    return (
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda deserunt aliquid minus deleniti eius. Aperiam consectetur, provident eius soluta, nihil iusto veritatis aliquid sit laboriosam officiis consequatur accusantium optio explicabo.</p>
+        </div>
+    )
+}
+
+export default parrafo
